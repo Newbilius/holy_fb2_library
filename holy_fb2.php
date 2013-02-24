ac79ef5f7ee194b61a31a94682be1d226f86dd77
Newbilius <newbilius@gmail.com>
2013-02-24 21:37:00 +0600

    fix

--- holy_fb2.php
+++ holy_fb2.php
@@ -1,5 +1,13 @@
 <?php
-
+/**
+ * Простейший генератор fb2-файлов.
+ *
+ * Лицензия - делайте с файлом что хотите, только не кидайтесь помидорами и автора из файлов не выкидывайте.
+ *
+ * @author     Моисеев Дмитрий aka Newbilius (Nubilius) <newbilius@gmail.com>
+ * @copyright  2013
+ * @link       https://github.com/Newbilius/habr_to_fb2
+ */
 include_once("strip_tags_smart/strip_tags_smart.php");
 
 function loadImageByType($filename, $type) {
